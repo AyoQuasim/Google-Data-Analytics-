f94dfe47725855bc943252134ecd04d3ab11b040
Adebayo Ayobami Quasim <110183791+AyoQuasim@users.noreply.github.com>
2022-09-30 21:10:01 +0100

    R language
    R programming language tutorials taken in google data analytics professional certificate.

---
+++ Data analytics with R programing language/Sales.R
@@ -0,0 +1,5 @@
+#R calculations
+Q1_sales <- 345661.32
+Q2_sales <- 435875.67
+midyear_sales <- Q2_sales + Q1_sales
+yearend_sales <- midyear_sales * 2
---
+++ Data analytics with R programing language/Variable.R
@@ -0,0 +1,6 @@
+#Let us define some Variables using R
+first_variable <-"My name is Adebayo Ayobami Quasim"
+second_variabble <- "I'm 30 years of age"
+#Adding vector to our functions
+vec_1 <- c(23, 3, 87.1, 15.9, 99)
+vec_1
